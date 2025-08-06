9da53151415874cd0b82482a0e3c831d433bbad2
FR <144460376+Megagoat05@users.noreply.github.com>
2025-08-06 01:55:22 +0100

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -17,7 +17,7 @@ import { AccountPage } from './components/Account';
 import { AuthProvider, useAuth } from './hooks/useAuth';
 import { WalletProvider, useWallet } from './hooks/useWallet';
 import { Account } from './types';
-import { InfoSections } from './components/InfoSections';
+import { InfoSections } from './components/infoSections';
 import { FreezeGuard } from "./components/FreezeGuard";
 import TermsOfService from './components/terms';
 
